6c637709741ef6113cd03b06948b50c0ab1b390b
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2026-07-02 23:08:18 -0300

    Montando estrutura primária do Detalhes.jsx

--- src/components/Detalhes.jsx
+++ src/components/Detalhes.jsx
@@ -12,11 +12,43 @@ const Detalhes = () => {
 
     return (
         <div className="detalhes-container">
+
             <div className="background-container"></div>
-            <button onClick={() => navigate(-1)}>← Voltar</button>
-            <img src={item.poster} alt={item.titulo} className="detalhes-poster" />
-            <h1>{item.titulo}</h1>
-            <p>{item.tipo === 'filme' ? '🎬 Filme' : '📺 Série'}</p>
+
+            <div className="detalhes-header">
+                <button className='sair-button' onClick={() => navigate(-1)}>Voltar</button>
+            </div>
+            
+            <div className="detalhes-info">
+                <div>
+                    <h1>{item.titulo}</h1>
+                    <h3>Gênero {item.tipo === 'filme' ? '🎬 Filme' : '📺 Série'}</h3>
+                    <div style={{display: 'flex'}}>
+                        <h3 style={{marginRight:'10px'}}>Nome original</h3>
+                        <h4>NOME</h4>
+                    </div>
+                    <div style={{display: 'flex'}}>
+                        <h3 style={{marginRight:'10px'}}>Diretor</h3>
+                        <h4>NOME DO DIRETOR</h4>
+                    </div>
+                    <div style={{display: 'flex'}}>
+                        <h3 style={{marginRight:'10px'}}>Roteirista</h3>
+                        <h4>NOME</h4>
+                    </div>
+                    <div style={{display: 'flex'}}>
+                        <h3 style={{marginRight:'10px'}}>Data de Lançamento</h3>
+                        <h4>NOME</h4>
+                    </div>
+                </div>
+                
+                <img src={item.poster} alt={item.titulo} className="detalhes-poster" />
+
+            </div>
+            <h2>Principais atores</h2>
+            <div className="atores-container">
+                <p>CARDS DOS ATORES</p>
+            </div>
+            <p className="space"></p>
             <iframe 
                 width="1260" height="615" 
                 src={item.trailer}
