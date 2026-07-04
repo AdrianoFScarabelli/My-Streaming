5688c48c2e48424cde0183e40705636ff632dae3
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2026-07-03 23:10:56 -0300

    Adicionando primeiros dados dos Detalhes no Titulos.js

--- src/components/Detalhes.jsx
+++ src/components/Detalhes.jsx
@@ -12,46 +12,52 @@ const Detalhes = () => {
 
     return (
         <div className="detalhes-container">
-
             <div className="background-container"></div>
-
             <div className="detalhes-header">
                 <button className='sair-button' onClick={() => navigate(-1)}>Voltar</button>
             </div>
-            
             <div className="detalhes-info">
                 <div>
                     <h1>{item.titulo}</h1>
-                    <h3>Gênero {item.tipo === 'filme' ? '🎬 Filme' : '📺 Série'}</h3>
+                    <div style={{display: 'flex'}}>
+                        <h3 style={{marginRight:'10px'}}>Gênero</h3>
+                        <h4>{item.tipo === 'filme' ? '🎬 Filme' : '📺 Série'}</h4>
+                    </div>
                     <div style={{display: 'flex'}}>
                         <h3 style={{marginRight:'10px'}}>Nome original</h3>
-                        <h4>NOME</h4>
+                        <h4>{item.nmOriginal}</h4>
                     </div>
                     <div style={{display: 'flex'}}>
                         <h3 style={{marginRight:'10px'}}>Diretor</h3>
-                        <h4>NOME DO DIRETOR</h4>
+                        <h4>{item.nmDiretor}</h4>
                     </div>
                     <div style={{display: 'flex'}}>
                         <h3 style={{marginRight:'10px'}}>Roteirista</h3>
-                        <h4>NOME DO ROTEIRISTA</h4>
+                        <h4>{item.nmRoteirista[0]}, {item.nmRoteirista[1]}</h4>
                     </div>
                     <div style={{display: 'flex'}}>
                         <h3 style={{marginRight:'10px'}}>Data de Lançamento</h3>
-                        <h4>DATA DE LANÇAMENTO DO FILME</h4>
+                        <h4>{item.dtLancamento}</h4>
                     </div>
                 </div>
-                
                 <img src={item.poster} alt={item.titulo} className="detalhes-poster" />
-
             </div>
             <h2>Principais atores</h2>
             <div className="atores-container">
                 <div className="atores-card">
-                    <img className="atores-image" src={item.atores} alt="imagem do ator" />
-                    <h3>{item.titulo}</h3>
+                    <img className="atores-image" src={item.ftAtores[0]} alt="imagem do ator" />
+                    <h3>{item.nmAtores[0]}</h3>
+                </div>
+                <div className="atores-card">
+                    <img className="atores-image" src={item.ftAtores[1]} alt="imagem do ator" />
+                    <h3>{item.nmAtores[1]}</h3>
+                </div>
+                <div className="atores-card">
+                    <img className="atores-image" src={item.ftAtores[2]} alt="imagem do ator" />
+                    <h3>{item.nmAtores[2]}</h3>
                 </div>
             </div>
-            <p className="space"></p>
+            <h2>Assista ao trailer</h2>
             <iframe 
                 width="1260" height="615" 
                 src={item.trailer}
